a81c8c26d870d4fef2d09f060a65bde52287401b
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-13 10:57:26 -0800

    adding pint function

--- monty.h
+++ monty.h
@@ -45,6 +45,7 @@ stack_t *globalStack;
 /* PROTOTYPES */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 void get_op(char *opcode, stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 
