d5c499f4b8cb93dd6b9c9ae62a01a90877045754
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-14 06:54:25 -0800

    testing format specifier

--- swap.c
+++ swap.c
@@ -7,7 +7,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if (topNode == NULL || topNode->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = topNode->n;
