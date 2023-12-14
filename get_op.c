d3d0643288ac1d24cfb42e721b1aec1be6081672
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-14 06:36:41 -0800

    adding nop function

--- get_op.c
+++ get_op.c
@@ -10,6 +10,7 @@ void get_op(char *opcode, stack_t **stack, unsigned int line_number)
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
+		{"nop", nop},
 		{NULL, NULL}
 	};
 	while (instructions[i].opcode != NULL)
