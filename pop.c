072f6b8709206daf96fe6b6224ee55da38c60a65
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-14 06:52:44 -0800

    chnaging format specifier

--- pop.c
+++ pop.c
@@ -6,7 +6,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	oldNode = *stack;
